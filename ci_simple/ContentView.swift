55c7afa0fddcdacb2bab432a59aab6c26d514161
junchen <junchen.liu@jiamiantech.com>
2024-02-21 14:39:16 +0800

    Initial Commit

---
+++ ci_simple/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ci_simple
+//
+//  Created by sama 刘 on 2024/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ci_simple/ci_simpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ci_simpleApp.swift
+//  ci_simple
+//
+//  Created by sama 刘 on 2024/2/21.
+//
+
+import SwiftUI
+
+@main
+struct ci_simpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
